348adbd25108d949015b856a7529a239733f596c
dereck120120120@gmail.com <dereck120120120@gmail.com>
2020-11-09 18:41:35 -0400

    -

---
+++ SEjemplo01/PEjemplo01/principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	cout << "Dereck Fernando Baldiviezo Cruz" << endl;
+	cout << "Edad: 20 años" << endl;
+	cout << "Correo: fer120120120@gmail.com" << endl;
+	cout << "Ingenieria de Sistemas" << endl;
+	return 0;
+}
